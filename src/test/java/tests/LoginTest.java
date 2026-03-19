eb6bd3916ac71105d2f0ec06a6d2d949a92f8340
naveen18893-tech <naveen18893@gmail.com>
2026-03-19 16:15:18 +0530

    Create LoginTest.java

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    private By usernameField = By.id("Email");
+    private By passwordField = By.id("Password");
+    private By loginButton = By.xpath("//button[text()='Log in']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).clear();
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).clear();
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+}
