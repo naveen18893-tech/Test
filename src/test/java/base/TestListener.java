573c3fa63ef9a71b054553dbb831499dcd02061f
naveen18893-tech <naveen18893@gmail.com>
2026-03-19 17:43:54 +0530

    Update TestListener.java

--- src/test/java/base/TestListener.java
+++ src/test/java/base/TestListener.java
@@ -1,12 +1,14 @@
+
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 
 public class TestListener implements ITestListener {
+
     private static ExtentReports extent = ExtentManager.getInstance();
     private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
@@ -27,7 +29,16 @@ public class TestListener implements ITestListener {
     }
 
     @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    @Override
     public void onFinish(ITestContext context) {
         extent.flush();
     }
+
+    // Other methods can stay empty
+    @Override public void onStart(ITestContext context) {}
+    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
 }
