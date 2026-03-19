f8124af73daeb1d0e59eb839d2f333e859074a4c
naveen18893-tech <naveen18893@gmail.com>
2026-03-19 17:12:49 +0530

    Create ExtentManager.java

---
+++ src/test/java/base/ExtentManager.java
@@ -0,0 +1,43 @@
+package base;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+
+    // Other methods can stay empty
+    @Override public void onStart(ITestContext context) {}
+    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
+}
