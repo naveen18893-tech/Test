9ac95bc678b3b3cf2766244e7ebbaf5ed47a815c
naveen18893-tech <naveen18893@gmail.com>
2026-03-19 17:18:55 +0530

    Update ExtentManager.java

--- src/test/java/base/ExtentManager.java
+++ src/test/java/base/ExtentManager.java
@@ -1,43 +1,20 @@
 package base;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-public class TestListener implements ITestListener {
+public class ExtentManager {
+    private static ExtentReports extent;
 
-    private static ExtentReports extent = ExtentManager.getInstance();
-    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
-
-    @Override
-    public void onTestStart(ITestResult result) {
-        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
-        test.set(extentTest);
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "/target/ExtentReport.html";
+            ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
+            reporter.config().setReportName("Automation Test Report");
+            reporter.config().setDocumentTitle("Test Results");
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+        }
+        return extent;
     }
-
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        test.get().pass("Test Passed");
-    }
-
-    @Override
-    public void onTestFailure(ITestResult result) {
-        test.get().fail(result.getThrowable());
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        test.get().skip(result.getThrowable());
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-        extent.flush();
-    }
-
-    // Other methods can stay empty
-    @Override public void onStart(ITestContext context) {}
-    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
 }
